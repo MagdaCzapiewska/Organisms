1edc86968c9de8b0e953866b10219e7478d8ab25
mn438628 <maciek.nowotek@gmail.com>
2022-11-28 15:44:58 +0100

    laby i tak pewnie do poprawy

--- organism.h
+++ organism.h
@@ -4,6 +4,7 @@
 #include <concepts>
 #include <optional>
 #include <type_traits>
+#include <tuple>
 
 /*
 
@@ -64,7 +65,92 @@ constexpr std::tuple<Organism<species_t, sp1_eats_m, sp1_eats_p>,
         Organism<species_t, sp2_eats_m, sp2_eats_p>,
         std::optional<Organism<species_t, sp1_eats_m, sp1_eats_p>>>
 encounter(Organism<species_t, sp1_eats_m, sp1_eats_p> organism1,
-          Organism<species_t, sp2_eats_m, sp2_eats_p> organism2);
+          Organism<species_t, sp2_eats_m, sp2_eats_p> organism2) {
+    static_assert(!(!sp1_eats_m && !sp1_eats_p && !sp2_eats_m && !sp2_eats_p), "rosliny nie moga sie spotykac");
+    if (organism1.get_vitality() == 0 || organism2.get_vitality() == 0) {
+        return std::make_tuple(
+                Organism<species_t, sp1_eats_m, sp1_eats_p> (organism1.get_species(), organism1.get_vitality()),
+                Organism<species_t, sp2_eats_m, sp2_eats_p> (organism2.get_species(), organism2.get_vitality()),
+                std::nullopt);
+    }
+    if (organism1.get_species() == organism2.get_species()) {
+        if (sp1_eats_m == sp2_eats_m && sp1_eats_p == sp2_eats_p) {
+            return std::make_tuple(
+                    Organism<species_t, sp1_eats_m, sp1_eats_p> (organism1.get_species(), organism1.get_vitality()),
+                    Organism<species_t, sp2_eats_m, sp2_eats_p> (organism2.get_species(), organism2.get_vitality()),
+                    Organism<species_t, sp1_eats_m, sp2_eats_p>
+                            (organism1.get_species(),(organism1.get_vitality() + organism2.get_vitality()) / 2));
+        }
+    }
+    /*if (sp1_eats_m && sp1_eats_p) {
+        if (sp2_eats_m) {
+            //walcza
+        }
+        //zjada albo nie zjada
+    }
+    if (sp1_eats_m && !sp1_eats_p) {
+        if (sp2_eats_m) {
+            //walcza
+        }
+        if (sp2_eats_p) {
+            //zjada albo nie zjada
+        }
+        //sp2 jest roslina, wiec sp1 go nie zjada
+    }
+    if (!sp1_eats_m && sp1_eats_p) {
+        if (!sp2_eats_m && !sp2_eats_p) {
+            //zjada
+        }
+        if (sp2_eats_m) {
+            //org2 moze zjesc org1
+        }
+        //oboje jedza tylko rosliny wiec sie nie zjadaja
+    }*/
+    if (sp1_eats_m && sp2_eats_m) {
+        if (organism1.get_vitality() > organism2.get_vitality()) {
+            return std::make_tuple(
+                    Organism<species_t, sp1_eats_m, sp1_eats_p> (organism1.get_species(), organism1.get_vitality() + organism2.get_vitality() / 2),
+                    Organism<species_t, sp2_eats_m, sp2_eats_p> (organism2.get_species(), 0),
+                    std::nullopt);
+        }
+        if (organism2.get_vitality() > organism1.get_vitality()) {
+            return std::make_tuple(
+                    Organism<species_t, sp1_eats_m, sp1_eats_p> (organism1.get_species(), 0),
+                    Organism<species_t, sp2_eats_m, sp2_eats_p> (organism2.get_species(), organism2.get_vitality() + organism1.get_vitality() / 2),
+                    std::nullopt);
+        }
+        else {
+            return std::make_tuple(
+                    Organism<species_t, sp1_eats_m, sp1_eats_p> (organism1.get_species(), 0),
+                    Organism<species_t, sp2_eats_m, sp2_eats_p> (organism2.get_species(), 0),
+                    std::nullopt);
+        }
+    }
+    if ((!sp1_eats_p && !sp2_eats_m && !sp2_eats_p) || (!sp2_eats_p && !sp1_eats_m && !sp1_eats_p)) {
+        return std::make_tuple(
+                Organism<species_t, sp1_eats_m, sp1_eats_p> (organism1.get_species(), organism1.get_vitality()),
+                Organism<species_t, sp2_eats_m, sp2_eats_p> (organism2.get_species(), organism2.get_vitality()),
+                std::nullopt);
+    }
+    if (sp1_eats_p && !sp2_eats_m && !sp2_eats_p) {
+        return std::make_tuple(
+                Organism<species_t, sp1_eats_m, sp1_eats_p> (organism1.get_species(), organism1.get_vitality() + organism2.get_vitality()),
+                Organism<species_t, sp2_eats_m, sp2_eats_p> (organism2.get_species(), 0),
+                std::nullopt);
+    }
+    if (sp2_eats_p && !sp1_eats_m && !sp1_eats_p) {
+        return std::make_tuple(
+                Organism<species_t, sp1_eats_m, sp1_eats_p> (organism1.get_species(), 0),
+                Organism<species_t, sp2_eats_m, sp2_eats_p> (organism2.get_species(), organism2.get_vitality() + organism1.get_vitality()),
+                std::nullopt);
+    }
+    if (sp1_eats_m) {
+        //a zjada b
+    }
+    if(sp2_eats_m) {
+        //b zjada a
+    }
+}
 
 template <typename species_t, bool sp1_eats_m, bool sp1_eats_p, typename ... Args>
 constexpr Organism<species_t, sp1_eats_m, sp1_eats_p>
